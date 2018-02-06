5735472d5853a63ffe86755110a3919c9b7b3e8d
Ryan Allen <allenryan14@gmail.com>
2018-02-05 20:06:06 -0500

    add model names/fields as comments for potential blueprint moving forward :free: :pizza:

--- django/teams/models.py
+++ django/teams/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+# http://django-organizations.readthedocs.io/en/latest/cookbook.html#custom-org-with-simple-inheritance
+# Team
+# name
+# logo
+# goal_horn (FK=Track)
+#
+# TeamSeason
+# season (FK)
+# team (FK)
+# gm (FK=Player)
+# asst_gm (FK=Player)
+#
+# Player
+# user (FK)
+# nicknames (array)
+# projected_round
+# handedness
+# phone
+# drink_participation (boolean)
+# sub_only (boolean)
+#
+# PlayerStint
+# player (FK)
+# team (FK=TeamSeason)
+# number
+# positions (array)
+# start_date
+# end_date
+#
+# TradeGroup
+# date
+# notes
+#
+# Trade
+# group (FK)
+# player (FK)
+# from (FK=TeamSeason)
+# to (FK=TeamSeason)
---
+++ django/venues/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Location (Abstract)
+# point
+# address_line_1
+# address_line_2
+# city
+# state_province
+# postal
+#
+# Venue
+# location (FK)
+# name
+# phone_numbers (array)
+# manager
