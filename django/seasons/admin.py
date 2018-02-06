6cc97a38e3dc3dd8d5ddb75dcd388a19954a2e81
Ryan Allen <allenryan14@gmail.com>
2018-02-05 21:12:31 -0500

    add model for tracking seasons :free: :pizza:

--- django/seasons/admin.py
+++ django/seasons/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Season
+from main.admin import mnl_admin
+
+
+@admin.register(Season, site=mnl_admin)
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'start_date', 'end_date',]
